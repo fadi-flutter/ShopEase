31f83897109740dae0aef48321241a2486ac2e9c
Fahad Ali <115652088+fadi-flutter@users.noreply.github.com>
2023-05-05 18:26:50 +0500

    UI completed

--- lib/views/Dashboard/profile/screens/profile_screen.dart
+++ lib/views/Dashboard/profile/screens/profile_screen.dart
@@ -1,12 +1,71 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:get/get.dart';
+import 'package:shopease/utilities/app_colors.dart';
+import 'package:shopease/utilities/app_textstyle.dart';
+import 'package:shopease/views/Dashboard/profile/screens/setttings_screen.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.bgColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.white,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          'My profile',
+          style: AppTextStyle.mediumBlack20,
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 15),
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const CircleAvatar(
+                backgroundColor: AppColors.black,
+                radius: 45,
+              ),
+              Text(
+                'Fahad Ali',
+                style: AppTextStyle.mediumBlack24,
+              ),
+              Text(
+                'fahadalicspb3010@gmail.com',
+                style: AppTextStyle.regularBlack14.copyWith(
+                  color: AppColors.grey,
+                ),
+              ),
+              const SizedBox(height: 20),
+              ListTile(
+                tileColor: AppColors.white,
+                title: Text(
+                  'Settings',
+                  style: AppTextStyle.mediumBlack16,
+                ),
+                subtitle: Text(
+                  'Change paswword & name...',
+                  style: AppTextStyle.regularWhite12
+                      .copyWith(color: AppColors.grey),
+                ),
+                trailing: GestureDetector(
+                  onTap: () {
+                    Get.to(() => const SettingScreen());
+                  },
+                  child: const Icon(
+                    Icons.arrow_forward_ios,
+                    color: AppColors.primary,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/views/Dashboard/profile/screens/setttings_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:shopease/utilities/app_colors.dart';
+import 'package:shopease/utilities/app_textstyle.dart';
+import 'package:shopease/widgets/auth_textfield.dart';
+import 'package:shopease/widgets/long_button.dart';
+
+class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: AppColors.bgColor,
+        appBar: AppBar(
+          leading: const BackButton(color: AppColors.black),
+          backgroundColor: AppColors.white,
+          centerTitle: true,
+          elevation: 0,
+          title: Text(
+            'Settings',
+            style: AppTextStyle.mediumBlack20,
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                CircleAvatar(
+                    backgroundColor: AppColors.primary,
+                    radius: 50,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.camera_alt,
+                        color: AppColors.white,
+                        size: 30,
+                      ),
+                    )),
+                const SizedBox(height: 7),
+                Text(
+                  'Change photo',
+                  style: AppTextStyle.regularBlack14,
+                ),
+                const SizedBox(height: 15),
+                const AuthTextField(
+                  text: 'Change name',
+                ),
+                const SizedBox(height: 05),
+                const AuthTextField(
+                  text: 'Change password',
+                ),
+                const Spacer(),
+                LongButton(onPressed: () {}, text: 'SAVE SETTINGS'),
+                const SizedBox(height: 12),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
